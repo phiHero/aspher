5840693be560590fc69e14dfa35a4b6a29a96196
Aspher <hphihung795@gmail.com>
2022-06-10 23:19:13 +0700

    design test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['bloganchoi.com'],
+    domains: ['bloganchoi.com', 'i.pinimg.com'],
   },
   typescript: {
     // !! WARN !!
