50e77420bdaf9da076514e9ca30837fb97de3818
Phi Hung Hoang <hphihung795@gmail.com>
2023-08-28 23:49:54 +0700

    search bar debounce and error handling

---
+++ client/src/components/headerBar/FuzzySearch/FuzzySearch.tsx
@@ -0,0 +1,121 @@
+import s from './FuzzySearch.module.scss';
+import Image from 'next/image';
+import axios from 'axios';
+import Link from 'next/link';
+import {
+  Dispatch,
+  RefObject,
+  SetStateAction,
+  useCallback,
+  useState,
+} from 'react';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import debounce from 'lodash.debounce';
+import CloseIcon from '@mui/icons-material/Close';
+import Loader from '../../loader/loader';
+
+export default function FuzzySearch({
+  searchBarVisible,
+  searchBarRef,
+  display,
+  setDisplay,
+}: {
+  searchBarVisible: () => void;
+  searchBarRef: RefObject<HTMLInputElement>;
+  display: boolean;
+  setDisplay: Dispatch<SetStateAction<boolean>>;
+}) {
+  const [searchData, setSearchData] = useState([]);
+  const [isLoading, setIsLoading] = useState<boolean | 'No data'>(false);
+
+  const fetcher = async (query: string) => {
+    try {
+      const { data } = await axios.post(`/api/film/search?q=${query}`);
+      setSearchData(data);
+      if (data?.length) {
+        setIsLoading(false);
+      } else {
+        setIsLoading('No data');
+      }
+    } catch (error) {
+      alert('Error!');
+      setIsLoading(false);
+    }
+  };
+  const getSearchResult = useCallback(debounce(fetcher, 1000), []);
+
+  const getSearchResultOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let query: string = e.target.value.trim();
+    if (query.length > 2) {
+      setIsLoading(true);
+      getSearchResult(query);
+    } else {
+      setIsLoading(false);
+      setSearchData([]);
+    }
+  };
+  return (
+    <div className={s.FuzzySearch} ref={searchBarRef}>
+      <div></div>
+      <div className={s.search}>
+        <button
+          className={s.iconButton}
+          onClick={searchBarVisible}
+          aria-label='Search'
+        >
+          <SearchRoundedIcon className={s.icon} />
+        </button>
+        <input
+          type='text'
+          placeholder='Search films...'
+          onChange={(e) => getSearchResultOnChange(e)}
+          onFocus={() => setDisplay(true)}
+        />
+
+        {display && (
+          <div className={s.searchResult}>
+            {isLoading === true ? (
+              <Loader height='40vh' size='2.5vmax' />
+            ) : isLoading === 'No data' ? (
+              <div id={s.noData}>Sorry, no data found!</div>
+            ) : (
+              searchData?.map((item: any, index: number) => (
+                <Link href={`/film/${item._id}`} key={index}>
+                  <a className={s.searchItem} onClick={searchBarVisible}>
+                    <div className={s.searchItemBgWrapper}>
+                      <Image
+                        className={s.searchItemImg}
+                        src={item.backgroundImg}
+                        layout='fill'
+                        objectFit='cover'
+                        alt={item.title + ' background image.'}
+                      />
+                    </div>
+                    <div className={s.searchItemDetail}>
+                      <h1 className={s.searchItemTitle}>{item.title}</h1>
+                      <div className={s.searchItemOtherName}>
+                        <span className={s.infoType}>Other name: </span>
+                        {item.otherName || 'none'}
+                      </div>
+                      <div className={s.searchItemEpisode}>
+                        <span className={s.infoType}>Number of episodes: </span>{' '}
+                        {(item.episode?.length || '?') + ' EP'}
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              ))
+            )}
+          </div>
+        )}
+      </div>
+      <button
+        className={s.iconButton}
+        onClick={searchBarVisible}
+        aria-label='Close search'
+      >
+        <CloseIcon className={s.icon} />
+      </button>
+    </div>
+  );
+}
--- client/src/components/headerBar/headerBar.tsx
+++ client/src/components/headerBar/headerBar.tsx
@@ -1,44 +1,17 @@
 import styles from './headerBar.module.scss';
 import sidebarStyles from '../sideBar/sideBar.module.scss';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import CloseIcon from '@mui/icons-material/Close';
-import { useRef, useState } from 'react';
-import axios from 'axios';
-import Image from 'next/image';
 import Link from 'next/link';
-import Loader from '../loader/loader';
 import { _filmData } from '@/interface/_film';
+import FuzzySearch from './FuzzySearch/FuzzySearch';
+import { useRef, useState } from 'react';
 
-const getSearchResult = async (query: string) => {
-  try {
-    const { data } = await axios.post(`/api/film/search?q=${query}`);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
 const HeaderBar = () => {
-  const [searchData, setSearchData] = useState([]);
-  const [display, setDisplay] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const searchBarRef = useRef<HTMLInputElement>(null);
+  const [display, setDisplay] = useState(false);
   const searchBarVisible = () => {
-    searchBarRef.current?.classList.toggle(styles.visible);
+    searchBarRef.current?.classList.toggle('visible');
     setDisplay(false);
-  };
-  const getSearchResultOnChange = async (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    let query: string = e.target.value.trim();
-    if (query.length > 3) {
-      setIsLoading(true);
-      let data = await getSearchResult(query);
-      setSearchData(data);
-      if (data) setIsLoading(false);
-    } else {
-      setIsLoading(false);
-      setSearchData([]);
-    }
   };
   return (
     <header className={styles.headerBar}>
@@ -72,68 +45,12 @@ const HeaderBar = () => {
           <SearchRoundedIcon className={styles.icon} />
         </button>
       </div>
-      <div className={styles.searchBar} ref={searchBarRef}>
-        <div></div>
-        <div className={styles.search}>
-          <button
-            className={styles.iconButton}
-            onClick={searchBarVisible}
-            aria-label='Search'
-          >
-            <SearchRoundedIcon className={styles.icon} />
-          </button>
-          <input
-            type='text'
-            placeholder='Search films...'
-            onChange={(e) => getSearchResultOnChange(e)}
-            onFocus={() => setDisplay(true)}
-          />
-
-          {display && (
-            <div className={styles.searchResult}>
-              {isLoading ? (
-                <Loader height='40vh' size='2.5vmax' />
-              ) : (
-                searchData?.map((item: _filmData, index: number) => (
-                  <Link href={`/film/${item._id}`} key={index}>
-                    <a className={styles.searchItem} onClick={searchBarVisible}>
-                      <div className={styles.searchItemBgWrapper}>
-                        <Image
-                          className={styles.searchItemImg}
-                          src={item.backgroundImg}
-                          layout='fill'
-                          objectFit='cover'
-                          alt={item.title + ' background image.'}
-                        />
-                      </div>
-                      <div className={styles.searchItemDetail}>
-                        <h1 className={styles.searchItemTitle}>{item.title}</h1>
-                        <div className={styles.searchItemOtherName}>
-                          <span className={styles.infoType}>Other name: </span>
-                          {item.otherName || 'none'}
-                        </div>
-                        <div className={styles.searchItemEpisode}>
-                          <span className={styles.infoType}>
-                            Number of episodes:{' '}
-                          </span>{' '}
-                          {(item.episode?.length || '?') + ' EP'}
-                        </div>
-                      </div>
-                    </a>
-                  </Link>
-                ))
-              )}
-            </div>
-          )}
-        </div>
-        <button
-          className={styles.iconButton}
-          onClick={searchBarVisible}
-          aria-label='Close search'
-        >
-          <CloseIcon className={styles.icon} />
-        </button>
-      </div>
+      <FuzzySearch
+        searchBarVisible={searchBarVisible}
+        display={display}
+        setDisplay={setDisplay}
+        searchBarRef={searchBarRef}
+      />
     </header>
   );
 };
