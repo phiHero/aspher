bd83a6cf79571937441b994d5996e6600d4771f2
Phi Hung Hoang <hphihung795@gmail.com>
2023-08-29 22:31:52 +0700

    Setup vitest with react-testing-lib

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
+    globals: false,
     environment: 'happy-dom',
+    setupFiles: './src/__tests__/setup.ts',
   },
 });
